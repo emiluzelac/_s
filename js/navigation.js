9b75691abbc0e9a97a029f39f0f65829698e6447
Emil Uzelac <emil@uzelac.me>
2013-07-04 22:09:06 -0500

    button.className space
    button.className had a space after the single quotes. This would disable the small menu toggle :)

--- js/navigation.js
+++ js/navigation.js
@@ -16,11 +16,11 @@
 			menu.className = 'nav-menu';
 
 		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
-			button.className = button.className.replace( ' toggled-on', '' );
-			menu.className = menu.className.replace( ' toggled-on', '' );
+			button.className = button.className.replace( 'toggled-on', '' );
+			menu.className = menu.className.replace( 'toggled-on', '' );
 			container.className = container.className.replace( 'main-small-navigation', 'navigation-main' );
 		} else {
-			button.className += ' toggled-on';
+			button.className += 'toggled-on';
 			menu.className += ' toggled-on';
 			container.className = container.className.replace( 'navigation-main', 'main-small-navigation' );
 		}
